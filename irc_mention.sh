dad93fc78aae0aef9ac6dc837ebbf479018f940e
root <root@fedora30.example.org>
2019-06-06 18:30:59 -0400

    first commit

---
+++ irc_mention.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+USER=prayther
+DATE=$(/usr/bin/date +"%b %d")
+
+if [ -f ./log/irc_last.log ];then
+	touch ./log/irc_last.log
+fi
+
+grep -i $USER * | grep "$DATE" > ./log/irc_current.log
+
+diff ./log/irc_current.log ./log/irc_last.log
+if [ $? == 0 ];then
+exit 0
+fi
+
+cp ./log/irc_current.log ./log/irc_last.log && /usr/sbin/sendmail -t <<EOF
+From: prayther@localhost.localdomain
+To: prayther@gmail.com
+Subject: IRC mention
+Content-Type: text/plain
+
+$(cat ./log/irc_last.log)
+
+EOF
+
+#rm -f irc_current.log
+
