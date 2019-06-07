cbfa50d251a06192e51fb6ae0c7ce7d25b1e6bc7
root <root@fedora30.example.org>
2019-06-07 11:22:08 -0400

    seems to work

--- irc_mention.sh
+++ irc_mention.sh
@@ -1,26 +1,32 @@
 #!/bin/bash -x
 
+DIR=/home/apraythe/.config/hexchat/logs/redhat
 USER=prayther
 DATE=$(/usr/bin/date +"%b %d")
 
-if [ -f ./log/irc_last.log ];then
-	touch ./log/irc_last.log
+if [ ! -f /tmp/irc_current.log ];then
+	touch /tmp/irc_current.log
 fi
 
-rm -f ./log/irc_current.log
-grep -i $USER * | grep "$DATE" >> ./log/irc_current.log
+if [ ! -f /tmp/irc_last.log ];then
+	touch /tmp/irc_last.log
+fi
 
-diff ./log/irc_current.log ./log/irc_last.log
+grep --directories=skip -i ${USER} ${DIR}/* | grep "${DATE}" > /tmp/irc_current.log
+
+diff /tmp/irc_current.log /tmp/irc_last.log
 if [ $? == 0 ];then
 exit 0
 fi
 
-cp ./log/irc_current.log ./log/irc_last.log && /usr/sbin/sendmail -t <<EOF
+mv /tmp/irc_current.log /tmp/irc_last.log
+
+/usr/sbin/sendmail -t <<EOF
 From: prayther@localhost.localdomain
 To: prayther@gmail.com
 Subject: IRC mention
 Content-Type: text/plain
 
-$(cat ./log/irc_last.log)
+$(cat /tmp/irc_last.log)
 
 EOF
